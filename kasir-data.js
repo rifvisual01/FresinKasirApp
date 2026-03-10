590974045b70e26a194a516a7a99c849fb4424d8
rifvisual01 <rifvisual01@gmail.com>
2026-03-10 14:45:53 +0700

    Create kasir-data.js

---
+++ kasir-data.js
@@ -0,0 +1,29 @@
+// kasir-data.js
+window.LOCAL_CASHIERS = [
+  {
+    name: 'Arzha',
+    pin: '000',
+    role: 'kasir',
+    active: true
+  },
+  
+   {
+    name: 'Izza',
+    pin: '000',
+    role: 'kasir',
+    active: true
+  },
+  
+   {
+    name: 'Afdhal',
+    pin: '000',
+    role: 'kasir',
+    active: true
+  },
+  {
+    name: 'Admin',
+    pin: '9999',
+    role: 'admin',
+    active: true
+  }
+];
